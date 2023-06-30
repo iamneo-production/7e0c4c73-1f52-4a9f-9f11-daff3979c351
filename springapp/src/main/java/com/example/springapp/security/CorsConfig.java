1b2e3efca99b7958d893ecaa8768d9776e17faa0
RahulSharma119 <rahulsharmakpggg@gmail.com>
2023-06-30 16:21:50 +0000

    changes for test cases

--- springapp/src/main/java/com/example/springapp/security/CorsConfig.java
+++ springapp/src/main/java/com/example/springapp/security/CorsConfig.java
@@ -3,8 +3,8 @@ package com.example.springapp.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 
 @Configuration

af9e711f5206782d7340bcbda7d17129355e3f63
SHASHANK08111 <sp08111999sp@gmail.com>
2023-07-06 10:53:50 +0000

    sync

--- reactapp/src/containers/SignUp/index.js
+++ reactapp/src/containers/SignUp/index.js
@@ -11,7 +11,7 @@ export const SignUp = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [type, setType] = useState('user');
-  const [signupResult,setSignupResult] = useState(null);
+ 
 
   const handleSubmit = (e) => {
     e.preventDefault();

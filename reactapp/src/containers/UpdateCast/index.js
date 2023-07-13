ef4fdaa00f0fe21abc97db59007835313d490287
masamnikhil <masam.nikhil03@gmail.com>
2023-07-13 15:36:04 +0000

    changes in updatecast

--- reactapp/src/containers/UpdateCast/index.js
+++ reactapp/src/containers/UpdateCast/index.js
@@ -1,14 +1,33 @@
-import React, {useState} from 'react'
-import {UseNavigate} from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import {useNavigate, useParams} from 'react-router-dom'
 import axios from 'axios';
 
 const UpdateCast = () => {
-      const navigate = UseNavigate();
+      const navigate = useNavigate();
       const [role,setRole] = useState('user')
       const[Castid,setCastid]=useState('')
       const[Castname,setCastname]=useState('')
       const[Movieid,setMovieid]=useState('')
-      const[option,setOption]=useState('')
+      const[Casts,setCasts]=useState([])
+      const[Option,setOption]=useState("")
+
+      var {movieId} = useParams()
+
+      useEffect(() => {
+        movieId = movieId.trim();
+        var token = window.localStorage.getItem('token');
+        var user = JSON.parse(window.localStorage.getItem('user'));
+        if (token && user && (user['role'] === 'ADMIN')){
+          
+        }
+            
+    
+            
+              
+      },[])
+      
+      
+      
       const submit = async (e) => {
         e.preventDefault();
         
@@ -17,14 +36,15 @@ const UpdateCast = () => {
       <div>
          <form onSubmit={(e) =>submit(e)}>
                 <label>CastID</label>
-                <input type="text" placeholder="Enter CastID"/>
+                <input type="text" placeholder="Enter CastID" value = {Castid} onChange={(e) => setCastid(e.target.value)}/>
                 <label>Cast Name</label>
-                <input type="text" placeholder="Enter Name"/>
+                <input type="text" placeholder="Enter Name" value = {Castname} onChange={(e) => setCastname(e.target.value)}/>
                 <label>movieID</label>
-                <input type="text" placeholder="Enter MovieID"/>
+                <input type="text" placeholder="Enter MovieID" value = {Movieid} onChange={(e) => setMovieid(e.target.value)}/>
                 <label>Choose the option</label>
-                <select OnChange={(e) => optionselect(e)}>
-                  
+                <select value = {Option} onChange={(e) => setOption(e.target.value)}>
+                  <option value = "Add Cast">Add Cast</option>
+                  <option value = "Delete Cast">Delete Cast</option>
                 </select>
                 <div><button>submit</button></div>
             </form>

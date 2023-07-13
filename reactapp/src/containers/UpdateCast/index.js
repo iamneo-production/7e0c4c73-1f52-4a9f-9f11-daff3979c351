e52bbb0c3466f5526259ef880be5a6276c9f2f8c
masamnikhil <masam.nikhil03@gmail.com>
2023-07-13 14:13:57 +0000

    updatecast work

--- reactapp/src/containers/UpdateCast/index.js
+++ reactapp/src/containers/UpdateCast/index.js
@@ -5,6 +5,10 @@ import axios from 'axios';
 const UpdateCast = () => {
       const navigate = UseNavigate();
       const [role,setRole] = useState('user')
+      const[Castid,setCastid]=useState('')
+      const[Castname,setCastname]=useState('')
+      const[Movieid,setMovieid]=useState('')
+      const[option,setOption]=useState('')
       const submit = async (e) => {
         e.preventDefault();
         
@@ -18,6 +22,10 @@ const UpdateCast = () => {
                 <input type="text" placeholder="Enter Name"/>
                 <label>movieID</label>
                 <input type="text" placeholder="Enter MovieID"/>
+                <label>Choose the option</label>
+                <select OnChange={(e) => optionselect(e)}>
+                  
+                </select>
                 <div><button>submit</button></div>
             </form>
       </div>

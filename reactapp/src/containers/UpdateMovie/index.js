be4b037ebc543f85de0196538352ec4dcddef992
AjayR23 <ajayjackson2332@gmail.com>
2023-07-14 14:07:54 +0000

    commit

--- reactapp/src/containers/UpdateMovie/index.js
+++ reactapp/src/containers/UpdateMovie/index.js
@@ -97,7 +97,7 @@ export const UpdateMovie = (props) => {
           setPoster(e.target.files[0]);
         }} />
         <br />
-        <button className='link-btn' type='submit'>Submit</button>
+        <button  type='submit'>Submit</button>
       </form>
     </div>
   )

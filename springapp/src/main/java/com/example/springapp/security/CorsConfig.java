2baa4a99e8cc92f8370c4eb1b6c55ef218dd7379
Ananthbarath <ananthbarath2001@gmail.com>
2023-07-14 16:29:06 +0000

    update changes

--- springapp/src/main/java/com/example/springapp/security/CorsConfig.java
+++ springapp/src/main/java/com/example/springapp/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("PUT", "DELETE","GET","POST");
-        ;
+        
     }
     
     @Override

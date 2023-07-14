2b5a29dccfa89a4d457e01f29fe53dc2eb5ec2b0
Ananthbarath <ananthbarath2001@gmail.com>
2023-07-14 15:23:46 +0000

    updatereview

--- reactapp/src/containers/UpdateReview/index.js
+++ reactapp/src/containers/UpdateReview/index.js
@@ -1,124 +1,267 @@
+// import axios from 'axios';
+// import React,{ useEffect,useState } from 'react';
+// import { useParams } from 'react-router-dom';
+
+// const url = process.env.REACT_APP_BACKEND_URL+'review';
+
+// export const UpdateReview = (props) => {
+
+//   var { reviewId } = useParams();
+//   const [movie,setMovie]=useState(null);
+//   const [review, setReview] = useState(null);
+//   const [reviewText, setReviewText] = useState('');
+//   const [rating, setRating] = useState('');
+
+//   useEffect(() => {
+//     reviewId = reviewId.trim();
+//     if (reviewId) {
+//       axios.get(url + "?id="+reviewId).then((response) => {
+//         setReview(response.data);
+//         setReviewText(response.data.reviewText);
+//         setRating(response.data.rating);
+//         setMovie(response.data.moive);
+       
+//       })
+//     }
+//   }, []);
+
+//   const handleDelete = (e) => {
+//     if (window.confirm("Are you sure you want to delete the Post by " + review.userId)) {
+//       axios.delete(url + reviewId, {
+//         headers: {
+//           'Authorization': `Bearer ${window.localStorage.getItem('token')}`
+//         }
+//       }).then((response) => {
+//         if (response.status == 200) {
+//           alert("Successfully Deleted the Post");
+//           window.location.href = process.env.REACT_APP_FRONTEND_URL+'movie/' + review.movieId.movieId;
+//         } else {
+//           alert("unable to Successfully delete the Post");
+//         }
+//       }).catch((err) => {
+//         alert(err + " unable to Successfully delete the Post");
+//         if (err.response.status == 401) {
+//           window.location.href = process.env.REACT_APP_FRONTEND_URL+'signin';
+//         }
+//       })
+//     }
+//   }
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const formdata = new FormData();
+//     if (reviewText && reviewText.trim() != '') formdata.append('reviewText', reviewText);
+//     if (rating && rating != '') formdata.append('rating', rating);
+//     if (reviewText == review.reviewText && rating == review.rating) {
+//       alert("Nothing to update");
+//     }
+//     else {
+//       if (window.confirm("Are you sure you want to Update the Post by " + review.userId)) {
+//         axios.put(url + reviewId, formdata, {
+//           headers: {
+//             'Authorization': `Bearer ${window.localStorage.getItem('token')}`
+//           }
+//         }).then((response) => {
+//           alert("Review post updated successfully");
+//           window.location.href = process.env.REACT_APP_FRONTEND_URL+'movie/' + review.movieId.movieId;
+//         }).catch((err) => {
+//           alert("Unable to Update the Review Post")
+//           if (err.response.status == 401) {
+//             window.location.href = process.env.REACT_APP_FRONTEND_URL+'signin';
+//           }
+//         })
+//       }
+//     }
+
+//   }
+
+
+
+//   return (
+//     <div className="container">
+//     <h3 className="title">Update Review Of{review && review.userId && review.userId.name}</h3>
+//       <div key={review.userId} className="review-item">
+//         {/* <div className='image-container'>
+//           <img src={process.env.REACT_APP_BACKEND_URL+'image/'+movie.poster} />
+//         </div> */}
+//         <h2 className="movie-title">{review.userId.movieName}</h2>
+//         <p className="username">Username:{review.userId.name}</p>
+//         <textarea 
+//         className="review-text"
+//         value={review.reviewText}
+//         onChange={(e) => { handleSubmit(e.target.value);}}
+//         style={{
+// 	          height: `${Math.max(2, Math.ceil(review.reviewText.length / 50))}em`,
+//               width: '700px',
+//               resize: 'none',
+//             }}
+//         />
+//         <div className="rating-container">
+//           <p className="rating-label">Rating:</p>
+//           <div className="star-rating">
+//           {[...Array(5)].map((_,index) =>(
+//           <span 
+//             key={index}
+//             className={`star ${index<rating ? 'filled' : 'empty'}`}
+//             onClick={(e) => handleSubmit(e.target.value)}
+//             style={{cursor:'pointer'}}
+//           >
+//           &#9733;
+//           </span> 
+//         ))}
+//         </div>
+//       </div>
+//     <div className="button-container">
+//         <button className="update-btn"
+//         onClick={handleSubmit}> Submit </button>
+//         <button className="delete-btn" onClick={handleDelete}>
+//         Delete
+//         </button>
+//     </div>
+//   </div>
+// </div>
+// )
+
+// }
 import axios from 'axios';
-import React,{ useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './index.css';
 
-const url = process.env.REACT_APP_BACKEND_URL+'review/'
+const url = process.env.REACT_APP_BACKEND_URL + 'review/';
 
-export const UpdateReview = (props) => {
-
-  var { reviewId } = useParams();
+export const UpdateReview = () => {
+  
+  const { reviewId } = useParams();
   const [movie,setMovie]=useState(null);
   const [review, setReview] = useState(null);
   const [reviewText, setReviewText] = useState('');
   const [rating, setRating] = useState('');
 
   useEffect(() => {
-    reviewId = reviewId.trim();
-    if (reviewId) {
-      axios.get(url + reviewId).then((response) => {
-        setReview(response.data);
-        setReviewText(response.data.reviewText);
-        setRating(response.data.rating);
-       
-      })
-    }
-  }, []);
+    const fetchReview = async () => {
+      try {
+        const response = await axios.get(`${url}?id=${reviewId}`);
+        console.log(response.data[0]);
+        setReview(response.data[0]);
+        setReviewText(response.data[0].reviewNote);
+        setRating(response.data[0].rating);
+        setMovie(response.data[0].movie);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  const handleDelete = (e) => {
-    if (window.confirm("Are you sure you want to delete the Post by " + review.userId.name)) {
-      axios.delete(url + reviewId, {
-        headers: {
-          'Authorization': `Bearer ${window.localStorage.getItem('token')}`
-        }
-      }).then((response) => {
-        if (response.status == 200) {
-          alert("Successfully Deleted the Post");
-          window.location.href = process.env.REACT_APP_FRONTEND_URL+'movie/' + review.movieId.movieId;
-        } else {
-          alert("unable to Successfully delete the Post");
-        }
-      }).catch((err) => {
-        alert(err + " unable to Successfully delete the Post");
-        if (err.response.status == 401) {
-          window.location.href = process.env.REACT_APP_FRONTEND_URL+'signin';
-        }
-      })
+    if (reviewId) {
+      fetchReview();
     }
-  }
+  }, [reviewId]);
+
+  const handleDelete =  () => {
+    try {
+      const confirmation = window.confirm(
+        `Are you sure you want to delete the Post by ${review?.userId}`
+      );
+      if (confirmation) {
+       axios.delete(`${url}?id=${reviewId}`, {
+          headers: {
+            'Authorization': `Bearer ${window.localStorage.getItem('token')}`,
+          },
+        });
+        alert('Successfully Deleted the Post');
+        window.location.href = process.env.REACT_APP_FRONTEND_URL;
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Unable to Successfully delete the Post');
+      if (error.response && error.response.status === 401) {
+        window.location.href = process.env.REACT_APP_FRONTEND_URL + 'signin';
+      }
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const formdata = new FormData();
-    if (reviewText && reviewText.trim() != '') formdata.append('reviewText', reviewText);
-    if (rating && rating != '') formdata.append('rating', rating);
-    if (reviewText == review.reviewText && rating == review.rating) {
-      alert("Nothing to update");
-    }
-    else {
-      if (window.confirm("Are you sure you want to Update the Post by " + review.userId.name)) {
-        axios.put(url + reviewId, formdata, {
-          headers: {
-            'Authorization': `Bearer ${window.localStorage.getItem('token')}`
-          }
-        }).then((response) => {
-          alert("Review post updated successfully");
-          window.location.href = process.env.REACT_APP_FRONTEND_URL+'movie/' + review.movieId.movieId;
-        }).catch((err) => {
-          alert("Unable to Update the Review Post")
-          if (err.response.status == 401) {
-            window.location.href = process.env.REACT_APP_FRONTEND_URL+'signin';
-          }
-        })
-      }
-    }
+    formdata.append('reviewId',reviewId);
+    formdata.append('reviewNote','awesome');
+  //   if (reviewText && reviewText.trim() !== '') {formdata.append('reviewNote', reviewText);
+  // console.log(reviewText);}
+  
+    // if (rating && rating !== '') {formdata.append('rating', rating); console.log(rating);}
+    // if (reviewText === review?.reviewNote && rating === review?.rating) {
+    //   alert('Nothing to update');
+    // } else {
+      // try {
+        const confirmation = window.confirm(
+          `Are you sure you want to Update the Post by ${review?.userId}`
+        );
+        if (confirmation) {
+           axios.put('https://8080-caefaeafdaedadeeaffeddeafeaeaadbdbabf.project.examly.io/review', formdata, {
+            headers: {
+              'Authorization': `Bearer ${window.localStorage.getItem('token')}`
+            },
+          }).catch((err)=>{console.log(err);});
+          alert('Review post updated successfully');
+          //window.location.href = process.env.REACT_APP_FRONTEND_URL;
+        }
+  //     } catch (error) {
+  //       console.error(error);
+  //       alert('Unable to Update the Review Post');
+  //       if (error.response && error.response.status === 401) {
+  //         window.location.href = process.env.REACT_APP_FRONTEND_URL + 'signin';
+  //       }
+  //     }
+  //   }
+  };
 
+  if (!review) {
+    return <div>Loading...</div>;
   }
-
-
 
   return (
     <div className="container">
-    <h3 className="title">Update Review Of{review && review.userId&&review.userId.name}</h3>
-      <div key={review.userId} className="review-item">
+      <h3 className="title">Update Review Of {review?.userId}</h3>
+      <div className="review-item">
         <div className='image-container'>
-          <img src={process.env.REACT_APP_BACKEND_URL+'image/'+movie.poster} />
-        </div>
-        <h2 className="movie-title">{review.userId.movieName}</h2>
-        <p className="username">Username:{review.userId.name}</p>
-        <textarea 
-        className="review-text"
-        value={review.reviewText}
-        onChange={(e) => { setReviewText(e.target.value);}}
-        style={{
-	          height: `${Math.max(2, Math.ceil(review.reviewText.length / 50))}em`,
-              width: '700px',
-              resize: 'none',
-            }}
+           <img src={process.env.REACT_APP_BACKEND_URL+'image/'+movie.poster} />
+         </div>
+        <h2 className="movie-title">{review?.movie?.title}</h2>
+        <p className="username">Username: {review?.userId}</p>
+        <textarea
+          className="review-text"
+          value={reviewText}
+          onChange={(e) => setReviewText(e.target.value)}
+          style={{
+            height: `100px`,
+            width: '700px',
+            resize: 'none',
+          }}
         />
         <div className="rating-container">
           <p className="rating-label">Rating:</p>
           <div className="star-rating">
-          {[...Array(5)].map((_,index) =>(
-          <span 
-            key={index}
-            className={`star ${index<rating ? 'filled' : 'empty'}`}
-            onClick={(e) => setRating(e.target.value)}
-            style={{cursor:'pointer'}}
-          >
-          &#9733;
-          </span> 
-        ))}
+            {[...Array(5)].map((_, index) => (
+              <span
+                key={index}
+                className={`star ${index < rating ? 'filled' : 'empty'}`}
+                onClick={() => setRating(index + 1)}
+                style={{ cursor: 'pointer' }}
+              >
+                &#9733;
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className="button-container">
+          <button className="update-btn" onClick={handleSubmit}>
+            Submit
+          </button>
+          <button className="delete-btn" onClick={handleDelete}>
+            Delete
+          </button>
         </div>
       </div>
-    <div className="button-container">
-        <button className="update-btn"
-        onClick={handleSubmit}> Submit </button>
-        <button className="delete-btn" onClick={handleDelete}>
-        Delete
-        </button>
     </div>
-  </div>
-</div>
-)
-
-}
+  );
+};

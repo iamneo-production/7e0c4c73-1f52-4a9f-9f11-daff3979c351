b66f4e5926c1e42496bc2ae0dc5ddcb58056e9e5
RahulSharma119 <rahulsharmakpggg@gmail.com>
2023-07-20 19:25:46 +0000

    added fetching of casts for a movie in springapp

--- reactapp/src/Components/CastCard/index.js
+++ reactapp/src/Components/CastCard/index.js
@@ -6,7 +6,7 @@ export const CastCard = (props) => {
   const { cast } = props;
   return (
     <div className='cardContainer'>
-      <img src={'http://localhost:8080/image/' + cast.poster} className='image' />
+      <img src={process.env.REACT_APP_BACKEND_URL+'image/' + cast.poster} className='image' />
       <h3 className='name' >{cast.name}</h3>
     </div>
   )

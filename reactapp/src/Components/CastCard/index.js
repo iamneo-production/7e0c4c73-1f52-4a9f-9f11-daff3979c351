418d9ed9e4aa3d43fcfadbcbc50890e4b6d4f879
masamnikhil <masam.nikhil03@gmail.com>
2023-07-23 08:40:32 +0000

    updated cast

--- reactapp/src/Components/CastCard/index.js
+++ reactapp/src/Components/CastCard/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './index.css';
 
-export const CastCard = ({cast}) => {
+export const CastCard = ({cast,handleDelete}) => {
   return (
     <div className='cast-item'>
-      <img src={process.env.REACT_APP_BACKEND_URL+'image/' + cast.poster} className='image' />
+      <img src={process.env.REACT_APP_BACKEND_URL+'image/' + cast.poster} className='imge' />
       <p className='cast-name' >{cast.name}</p>
+      <button className='del-btn' onClick={(e) => handleDelete(cast)}>X</button>
     </div>
   );
 };
--- reactapp/src/Components/CastCard/index2.js
+++ reactapp/src/Components/CastCard/index2.js
@@ -7,8 +7,8 @@ export const Card = (props) => {
   return (
     <div className='cardContainer'>
       <img src={process.env.REACT_APP_BACKEND_URL+'image/' + cast.poster} className='image' />
-      <button className="btn" onClick={(e)=>handleAdd(cast.castId)}>+</button>
-      <h3 className='name' >{cast.name}</h3>
+      <button className="add-btn" onClick={(e)=>handleAdd(cast)}>+</button>
+      <h3 className='cast-name' >{cast.name}</h3>
     </div>
   )
 
